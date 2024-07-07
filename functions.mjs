b112f0a77c61a05206a7b4993d7d5d248af89971
ayewhosthat <alan.notiverson@gmail.com>
2024-07-07 14:13:13 -0400

    Fetch current + 7 day weather, and output it to the console

--- functions.mjs
+++ functions.mjs
@@ -1,10 +1,24 @@
 import { MY_API_KEY } from "./api-key.mjs";
 async function getWeatherData(city) {
+    // function to get hourly forecast for current day
     const url = `https://api.weatherapi.com/v1/current.json?key=${MY_API_KEY}&q=${city}`;
     const response = await fetch(url, {mode: "cors"});
-    // since we have used await this returns a promise, we can then use .then on the promise to console.log
-    // the weather data on a successful query
-    response.json().then((response) => console.log(response));
+    const currentdata = await response.json();
+    return currentdata;    
 }
 
-export { getWeatherData };
+async function getWeatherForecast(city) {
+    // function to get 7 day forecast (exclusive of current day)
+    const daily_url = `https://api.weatherapi.com/v1/forecast.json?key=${MY_API_KEY}&q=${city}&days=7`;
+    const responseDaily = await fetch(daily_url, {mode: "cors"});
+    const forecastdata = await responseDaily.json();
+    return forecastdata;
+}
+
+function processWeatherData(response) {
+    // now we get the information we want to display for our app and process it
+    // maybe return a custom object????
+    response.then()
+}
+
+export { getWeatherData, getWeatherForecast };
--- main.mjs
+++ main.mjs
@@ -1,3 +1,7 @@
-import { getWeatherData } from "./functions.mjs";
+import { getWeatherData, getWeatherForecast } from "./functions.mjs";
+let currentCity = 'Mississauga';
 
-getWeatherData('Mississauga');
+const realtime = getWeatherData(currentCity);
+realtime.then(response => console.log(response));
+const forecast = getWeatherForecast(currentCity);
+forecast.then(response => console.log(response));
