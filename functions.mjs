906f50b5acd0832107131df9ec0a181cd8393e45
ayewhosthat <alan.notiverson@gmail.com>
2024-07-07 23:27:08 -0400

    Extract data needed for current weather section

--- functions.mjs
+++ functions.mjs
@@ -1,7 +1,7 @@
 import { MY_API_KEY } from "./api-key.mjs";
 async function getWeatherData(city) {
-    // function to get hourly forecast for current day
-    const url = `https://api.weatherapi.com/v1/current.json?key=${MY_API_KEY}&q=${city}`;
+    // function to get hourly forecast + current weather for current day
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${MY_API_KEY}&q=${city}`;
     const response = await fetch(url, {mode: "cors"});
     const currentdata = await response.json();
     return currentdata;    
@@ -15,10 +15,29 @@ async function getWeatherForecast(city) {
     return forecastdata;
 }
 
-function processWeatherData(response) {
+function processTodayWeatherData(response) {
     // now we get the information we want to display for our app and process it
     // maybe return a custom object????
-    response.then()
+    response.then((data) => {
+        const localTime = data['location']['localtime'];
+        const dateObject = new Date(localTime);
+        const currentWeather = {
+            currentTempC: data['current']['temp_c'],
+            currentTempF: data['current']['temp_f'],
+            feelsLikeC: data['current']['feelslike_c'],
+            feelsLikeF: data['current']['feelslike_f'],
+            humidity: data['current']['humidity'] + '%',
+            condition: data['current']['condition']['text'],
+            uv: data['current']['uv'],
+            windSpeed: data['current']['wind_kph'] + 'kph',
+            windDirection: data['current']['wind_dir'],
+            cityName: data['location']['name'],
+            date: Date(data['location']['localtime']).toString().split(' ').slice(0, 3).join(' '),
+            time: dateObject.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+        }
+        console.log(data);
+        console.log(currentWeather);
+    })
 }
 
-export { getWeatherData, getWeatherForecast };
+export { getWeatherData, getWeatherForecast, processTodayWeatherData };
