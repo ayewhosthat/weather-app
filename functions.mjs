89648dd6623b5cbaa04c89eca733eb0c168b7c48
ayewhosthat <alan.notiverson@gmail.com>
2024-07-08 21:28:18 -0400

    Extract daily weather for next 7 days (not including current day)

--- functions.mjs
+++ functions.mjs
@@ -9,7 +9,7 @@ async function getWeatherData(city) {
 
 async function getWeatherForecast(city) {
     // function to get 7 day forecast (exclusive of current day)
-    const daily_url = `https://api.weatherapi.com/v1/forecast.json?key=${MY_API_KEY}&q=${city}&days=7`;
+    const daily_url = `https://api.weatherapi.com/v1/forecast.json?key=${MY_API_KEY}&q=${city}&days=8`;
     const responseDaily = await fetch(daily_url, {mode: "cors"});
     const forecastdata = await responseDaily.json();
     return forecastdata;
@@ -35,7 +35,6 @@ function processTodayWeatherData(response) {
         currentWeather['cityName'] = data['location']['name']
         currentWeather['date'] = Date(data['location']['localtime']).toString().split(' ').slice(0, 3).join(' ')
         currentWeather['time'] = dateObject.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-        
 
         for (const hourInfo of data['forecast']['forecastday'][0]['hour']) {
             const timeOfDay = hourInfo['time'];
@@ -54,4 +53,23 @@ function processTodayWeatherData(response) {
     }
 }
 
-export { getWeatherData, getWeatherForecast, processTodayWeatherData };
+function process7DayWeather(response) {
+    const weekWeather = [];
+    response.then((data) => {
+        for (let i = 1; i < data['forecast']['forecastday'].length; i++) {
+            const day = data['forecast']['forecastday'][i];
+            weekWeather.push({
+                condition: day['day']['condition']['text'],
+                maxTempC: day['day']['maxtemp_c'],
+                maxTempF: day['day']['maxtemp_f'],
+                minTempC: day['day']['mintemp_c'],
+                minTempF: day['day']['mintemp_f'],
+                date: day['date'],
+                dayOfWeek: new Date(day['date']).toUTCString().split(',')[0]
+            })
+        }
+    })
+    return weekWeather;
+}
+
+export { getWeatherData, getWeatherForecast, processTodayWeatherData, process7DayWeather };
