67d73de843bd6c0e593999801d587fb779593031
Alan Hu <alan.notiverson@gmail.com>
2024-07-10 22:07:30 -0400

    Update currentPage which represents the slide to be shown

--- main.mjs
+++ main.mjs
@@ -1,6 +1,6 @@
 import { getWeatherData, getWeatherForecast, processTodayWeatherData, process7DayWeather } from "./functions.mjs";
 let currentCity = 'Mississauga';
-let currentPage = 1; // represents the current 'slide' of the hourly section that we're on
+let currentPage = 0; // represents the current 'slide' of the hourly section that we're on
 
 // add event listeners and functions to update the carousel to change what hours we can see 
 const page1 = document.getElementById('page-1');
@@ -9,9 +9,27 @@ const page3 = document.getElementById('page-3');
 const page4 = document.getElementById('page-4');
 
 // initial page is page1, set the display of others to none
-console.log(page1);
 page1.style.display = 'grid';
 page2.style.display = 'none';
 page3.style.display = 'none';
 page4.style.display = 'none';
 
+const moveRight = document.getElementById('move-right');
+moveRight.addEventListener('click', () => {
+    currentPage = (currentPage + 1) % 4;
+    console.log(currentPage);
+});
+
+const moveLeft = document.getElementById('move-left');
+moveLeft.addEventListener('click', () => {
+    if (currentPage === 0) {
+        currentPage = 3;
+    } else if (currentPage === 1) {
+        currentPage = 0;
+    } else if (currentPage === 2) {
+        currentPage = 1;
+    } else {
+        currentPage = 2;
+    }
+    console.log(currentPage)
+;})
